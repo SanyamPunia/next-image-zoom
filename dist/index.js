32d34859fd4f5224e8ae2a4c0866b63cc5ad30a3
SanyamPunia <lewarends@gmail.com>
2024-10-23 16:18:10 +0530

    commit hold

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ZoomWrapper = void 0;
-var ZoomWrapper_1 = require("./components/ZoomWrapper");
-Object.defineProperty(exports, "ZoomWrapper", { enumerable: true, get: function () { return ZoomWrapper_1.ZoomWrapper; } });
